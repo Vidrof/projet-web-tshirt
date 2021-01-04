7cb269294e894c5785eacf518212dac844d9adb0
anthoine <anthoine@etu.u-pec.fr>
2021-01-04 14:35:04 +0100

    html css

---
+++ client/components/Annonce.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="annonce">
+        <img class="tshirt_img" src="img/logo.jpg" alt="tshirt image">
+        <div class="texte">
+            <div class="titre">
+                <h3 id="titre_annonce">Tshirt de qualité</h3>
+                <div class="note"><h3>4.2 </h3><img class="star" src="img/star.svg" alt="little star"></div>
+            </div>
+            <div class="description">
+                <p>Un tshirt c’est quoi ? Un tshirt c’est dla peuf voilà cque c’est ok? jpense que la vie c’est des rencontres</p>
+            </div>
+            <button>En savoir plus</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #annonce{
+        margin: 20px auto;
+        width: 700px;
+        box-shadow: 0 0 19px rgba(0, 0, 0, 0.25);
+        border-radius: 17px;
+        display: flex;
+        flex-direction: row;
+        height: 250px;
+    }
+    .tshirt_img{
+        height: 250px;
+        width: 250px;
+        border-radius: 17px 0px 0px 17px;
+    }
+    .texte{
+        display: flex;
+        flex-direction: column;
+    }
+    .texte .titre{
+        display: flex;
+        flex-direction: row;
+    }
+    .titre #titre_annonce{
+        margin: 20px auto;
+    }
+    .note{
+        display: flex;
+        flex-direction: row;
+        float: left;
+        margin: auto 0;
+    }
+    .note h3{
+        width: min-content;
+    }
+    .star{
+        height: 30px;
+        width: 30px;
+    }
+    .description{
+        margin: 30px 30px;
+        font-weight: 300;
+    }
+    button{
+        background-color: #80FFC2;
+        color: #00A807;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 50px;
+        margin: 0 auto;
+    }
+</style>
+<script>
+module.exports = {
+	name:"Annonce"
+}
+</script>
---
+++ client/components/Tshirts.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h2>Des Tshirts ? La réponse est oui !</h2>
+        <div id="filtres">
+            <button>Recherche</button>
+        </div>
+        <annonce></annonce>
+    </div>
+</template>
+
+<style scoped>
+    h2{
+        margin: 20px 0;
+        text-align: center;
+        color: grey;
+    }
+    #filtres{
+        width: 500px;
+        height: 300px;
+        box-shadow: 0 0 19px rgba(0, 0, 0, 0.25);
+        border-radius: 17px;
+        margin: 0 auto;
+    }
+    #filtres button{
+        background-color: #4D74FF;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 50px;
+        border: none;
+    }
+    #filtres button:hover{
+        background-color: #416aff;
+    }
+    #filtres button:active{
+        background-color: #416aff;
+        box-shadow: inset 20px 20px 60px #375ad9, 
+            inset -20px -20px 60px #4b7aff;
+    }
+</style>
+<script>
+const Annonce = window.httpVueLoader('./components/Annonce.vue')
+
+module.exports = {
+    components : {
+        Annonce
+    },
+    created : function(){
+        console.log("test")
+    }
+}
+</script>
