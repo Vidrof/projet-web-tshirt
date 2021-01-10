cbe03a7f39435087121198c0243de7a3aa638ffa
TheGlu3guy <Glueguy@hotmail.fr>
2021-01-10 18:40:49 +0100

    changes

--- client/components/Vente.vue
+++ client/components/Vente.vue
@@ -1,40 +1,43 @@
 <template>
     <div>
         <div class="nouveau_produit">
-            <h2>Ajouter un nouveau produit à la vente</h2>
-            <form action="Ajouter un produit" method="post" onsubmit="return false" id="mon_form">
+            <h2>Poster mon Tshirt</h2>
+            <form @submit.prevent="addTshirt" id="mon_form">
                 <div class="ajouter_produit">
+                    <div class="field_description">
+                        <label for="description">Titre :</label>
+                        <input type="text" id="description" name="description" placeholder="Veuillez saisir un titre">
+                    </div>
+                    <div class="field_description">
+                        <label for="description">Description :</label>
+                        <input type="text" id="description" name="description" placeholder="Veuillez saisir une description">
+                    </div>
                     <div class="field_catégorie">
-                        <label for="Catégorie">Catégorie</label>
+                        <label for="Catégorie">Type</label>
                         <select id="Catégorie" name="catégorie">
-                            <option value="none" selected disabled hidden>Catégorie</option>
-                            <option value="col_rond">Col rond</option>
-                            <option value="col_v">Col V</option>
+                            <option value="none" selected disabled hidden>Type</option>
+                            <option v-for="type in types" :key="type.id_type" :value="type.id_type">{{type.nom}}</option>
                         </select>
                     </div>
                     <div class="field_couleur">
                         <label for="Couleur">Couleur</label>
-                        <select id="Couleur" name="couleur" multiple="yes">
-                            <option value="rouge">Rouge</option>
-                            <option value="jaune">Jaune</option>
-                            <option value="bleu">Bleu</option>
-                            <option value="vert">Vert</option>
-                            <option value="rouge">Blanc</option>
-                            <option value="jaune">Noir</option>
-                            <option value="bleu">Marron</option>
-                            <option value="vert">Rose</option>
-                        </select>
-                    </div>
-                    <div class="field_prix">
-                        <label for="prix">Prix (en euros) :</label>
-                        <input type="number" id="prix" name="prix" placeholder="Veuillez saisir un prix" required>
-
+                        <select id="Couleur" name="couleur">
+                            <option value="none" selected disabled hidden>Couleur</option>
+                            <option v-for="couleur in couleurs" :key="couleur.id_couleur" :value="couleur.id_couleur">{{couleur.nom}}</option>
                         </select>
                     </div>
                     <div class="field_photo">
                         <label for="photo">Photo(s) :</label>
                         <input type="file" id="photo" name="photo" placeholder="Photo" accept="image/png, image/jpeg">
                     </div>
+                    <!--
+                    <div class="field_prix">
+                        <label for="prix">Prix (en euros) :</label>
+                        <input type="number" id="prix" name="prix" placeholder="Veuillez saisir un prix" required>
+
+                        </select>
+                    </div>
+                    
                     <div class="field_quantité">
                         <label for="quantité">Quantité disponible :</label>
                         <input type="number" id="quantité" name="quantité" placeholder="Veuillez saisir une quantité" required>
@@ -43,10 +46,10 @@
                         <label for="description">Description :</label>
                         <input type="text" id="description" name="description" placeholder="Veuillez saisir votre description">
                     </div>
+                    -->
                 </div>
                 <div class="bottom">
-                    <button id='ajouter'>Ajouter mon produit à la vente</button>
-                    <button id='supprimer' onclick="supprimer_element()">Tout supprimer</button>
+                    <button>Poster</button>
                 </div>
             </form>
         </div>
@@ -75,20 +78,33 @@
 </template>
 
 <style scoped>
+    button{
+        background-color: #4D74FF;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 50px;
+        border: none;
+        margin-right: auto;
+        margin-left:auto;
+        margin-top:20px;
+        margin-bottom:20px;
+    }
+    button:hover{
+        background-color: #416aff;
+    }
+    button:active{
+        background-color: #416aff;
+        box-shadow: inset 20px 20px 60px #375ad9, 
+            inset -20px -20px 60px #4b7aff;
+    }
     select{
         border: none;
         height: 25px;
         width: 250px;
     }
-    .field_couleur select{
-        height: 60px;
-    }
     .nouveau_produit{
-        margin-left: auto;
-        margin-right: auto;
+        margin: 30px auto;
         width: 520px;
-        height: 420px;
-        margin-bottom: 2em;
         background-color: #ECF0F3;
         border-radius: 10px;
         box-shadow: 10px 10px 20px 0px #cdd3dd;
@@ -108,6 +124,7 @@
     }
     h2{
         text-align: center;
+        margin: 15px 0;
     }
     .bottom{
         margin-left: auto;
@@ -151,4 +168,30 @@
 </style>
 
 <script>
+module.exports = {
+        data () {
+            return {
+                types: [],
+                type: 0,
+                couleurs: [],
+                couleur: 0,
+            }
+        },
+        async mounted () {
+            console.log(this.isConnected)
+        },
+        async created(){
+            const result = await axios.get('/api/produits', {})
+            this.produits = result.data
+
+            const result2 = await axios.get('/api/labels', {})
+            this.labels = result2.data
+
+            const result3 = await axios.get('/api/annonce', {})
+            this.annonces = result3.data
+
+            const result4 = await axios.get('/api/regions', {})
+            this.regions = result4.data
+        },
+    }
 </script>
