156a909ae77c22d0fa9d85747582c9d7bc461a91
mathisbeau <mathis.beau@efrei.net>
2021-01-10 16:56:45 +0100

    desgin login et singup

--- client/components/Sign_up.vue
+++ client/components/Sign_up.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h2>Créer votre compte</h2>
         <div id="register">
+            <h2>Créer votre compte</h2>
             <form @submit.prevent="createUser()">
                 <div>
                     <label for="pseudo">Pseudo*</label>
@@ -17,6 +17,7 @@
                 </div>    
                 <div>
                     <label for="password">Mot de passe*</label>
+                    <p id="consigne">Votre mot de passe doit contenir 8 caractères, dont au moins une minuscule, une majuscule et un chiffre.</p>
                     <div class="field_password"> 
                         <input type="password" id="password" v-model="password" placeholder="Mot de passe">
                     </div>
@@ -27,10 +28,17 @@
                         <input type="password" id="cpassword" v-model="c_password" placeholder="Confirmation du mot de passe">
                     </div> 
                 </div>
+                <div class="required">
+                    <p>*Champs obligatoires</p>
+                </div>
                 <div id="bouton_créer">                   
                     <button type="submit">Créer un compte</button>
                 </div>
             </form>
+        </div>
+        <div id="login">
+            <h2> Vous avez déjà un compte ?<br>Connectez-vous</h2>
+            <p id="se_connecter"><router-link to='/log_in'>Se connecter</router-link></p>
         </div>
     </div>
 </template>
@@ -38,10 +46,14 @@
 <style scoped>
     #register{
         margin: 0 auto;
-        width : 200px;
+        width : 230px;
         margin-bottom: 2em;
+        margin-top: 5em;
     }
-
+    #login{
+        margin: 0 auto;
+        width: 330px;
+    }
     label{
         color: #004079;
         font-size: 14px;
@@ -56,16 +68,56 @@
     h2{
         text-align: center;
         color: #004079;
+        margin-bottom: 1.5em;
     }
-
+    button{
+        background-color: grey;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 50px;
+        border: none;
+        margin-top: .5em;
+    }
     .field_pseudo, .field_mail, .field_password, .field_cpassword{
         display: block;
         margin-bottom: .9em;
         margin-top: .2em;
     }
+    #consigne{
+        color: #6C6B6C;
+        font-size: smaller;
+        text-align: justify;
+    }
+    .required{
+        margin-left: auto;
+        margin-right: auto;
+        width: 200px;
+        color: #6C6B6C;
+        font-size: smaller;
+        text-align: justify;
+        margin-bottom: .9em;
+        margin-top: .2em;
+    }
+    #se_connecter{
+        text-align: center;
+    }
+    input{
+        background-color: white;
+        border: none;
+        border-bottom: 1px solid black;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: .5em;
+        height: 25px;
+        width: 200px;
+    }
 </style>
 
 <script>
+    const Log_in = window.httpVueLoader('./components/Log_in.vue')
+    const routes = [
+        { path: '/log_in', component: Log_in, meta :{title: 'Se connecter'}},
+    ]
     module.exports = {
         data () {
             return {
